30d86c46b368f9ed531d56029114340a58098bb4
Muhammad Haseeb <muhammadhaseeb6565@gmail.com>
2026-08-17 03:01:55 +0500

    Fix missing ChatPromptTemplate import in backend

--- backend/core/security_rag.py
+++ backend/core/security_rag.py
@@ -2,6 +2,7 @@ import os
 from dotenv import load_dotenv
 from langchain_groq import ChatGroq
 from langchain_core.messages import HumanMessage, SystemMessage
+from langchain_core.prompts import ChatPromptTemplate
 
 load_dotenv()
 
